61f3f94a1675ba5fd6f2a8b3ce2010ccc2a6c8bb
Clémentine Chazal <clementinechazal@MacBook-Air-de-admin.local>
2025-08-13 10:30:30 +0200

    normalizing flow

--- extensible_sampling.py
+++ extensible_sampling.py
@@ -26,23 +26,27 @@ class ExtensibleSampling:
         n, d = X.shape
         n_grid = n0 + int(jnp.sqrt(it))
         axis = jnp.linspace(l, u, n_grid)
+        #print(axis)
         meshes = jnp.meshgrid(*[axis]*d, indexing="ij")
-        grid_points = jnp.stack(meshes, axis=-1).reshape(-1, d)
+        grid_points = jnp.stack(meshes, axis=-1).reshape(-1, d) + 0.01 * random.normal(jax.random.PRNGKey(it), shape=(n_grid**d, d))  # Adding small noise to avoid exact duplicates
         KGD_grid = jnp.zeros(n_grid**d)
         for i in range(n_grid**d):
+            #print(self.KGD.evaluate(jnp.concatenate((X, grid_points[i][None,:]), axis=0)))
             KGD_grid = KGD_grid.at[i].set(self.KGD.evaluate(jnp.concatenate((X, grid_points[i][None,:]), axis=0)))
         min_idx = jnp.argmin(KGD_grid)
         return grid_points[min_idx]
     
-    #def MonteCarlo(self):
+    #def MonteCarlo_search(self):
+
+    #def GD_search(self,X):
          
         
 
-    def run_particles(self,x0,T,l = -5,u = 5,n0 = 100):
+    def run_particles(self,x0,T,l = -3,u = 3,n0 = 100):
         d = len(x0)
         X = jnp.zeros((T, d))
         X = X.at[0].set(x0)
-        for it in range(T-1):
+        for it in range(1,T):
             print("Iteration:", it)
-            X = X.at[it+1].set(self.grid_search(X[:it], l, u, n0, it))
+            X = X.at[it].set(self.grid_search(X[:it], l, u, n0, it))
         return X                        
