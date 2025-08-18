18fea8aae6be76e7c43bacd0883706a90a967188
Clémentine Chazal <clementinechazal@MacBook-Air-de-admin.local>
2025-08-18 21:25:37 +0200

    optimiser

--- variational_inference.py
+++ variational_inference.py
@@ -11,7 +11,37 @@ import time
 import os
 from jax import grad, random,vmap,tree_util
 from jax.flatten_util import ravel_pytree
+from flax import traverse_util
 from flax import linen as nn
+from flax import nnx
+import optax
+
+
+import flax.nnx as nnx
+
+class MLP(nnx.Module):
+    def __init__(self, input_dim, hidden_dims, output_dim, activation="tanh", *, rngs: nnx.Rngs):
+        self.layers = []
+        in_dim = input_dim
+
+        for h in hidden_dims:
+            self.layers.append(nnx.Linear(in_dim, h, rngs=rngs))
+            in_dim = h
+
+        self.out = nnx.Linear(in_dim, output_dim, rngs=rngs)
+        self.activation = activation
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+            if self.activation == "relu":
+                x = nnx.relu(x)
+            elif self.activation == "tanh":
+                x = jnp.tanh(x)
+            elif self.activation == "sigmoid":
+                x = nnx.sigmoid(x)
+
+        return self.out(x)
 
 class VariationalInference:
     def __init__(self, q0, L, k,d,layers,activation_function):
@@ -24,35 +54,79 @@ class VariationalInference:
         self.S_PQ = jit(lambda X: self.S_q0(X) - self.gradL(X))
         self.k_pq = GradientKernel(self.S_PQ, k)
         self.KGD = KernelGradientDiscrepancy(self.k_pq)
-        self.model_T = f.MLP(layers,self.d,activation_function)
+        self.model_T = MLP(self.d,layers,self.d,activation_function, rngs=nnx.Rngs(0)) 
+        self.optimizer = nnx.Optimizer(self.model_T, optax.adam(1e-3), wrt=nnx.Param)
 
-        key = random.PRNGKey(0)
-        params = self.model_T.init(key, jnp.zeros((self.d,)))  
-        self.unravel_fn = ravel_pytree(params)[1]
+        # key = random.PRNGKey(0)
+        # params = self.model_T.init(key, jnp.zeros((self.d,)))  
+        # self.unravel_fn = ravel_pytree(params)[1]
 
     
-    def push_T_theta(self,theta,W):
-         param = self.unravel_fn(theta)
-         return vmap(lambda w : self.model_T.apply(param, w))(W)
+    # def flatten_params_to_array(self,model):
+    #     params = nnx.state(model, nnx.Param)
         
-    def F(self,theta,key,n_sim,d):
-            W = random.normal(key, shape=(n_sim, d))
-            X = self.push_T_theta(theta,W)
+    #     flat_params = traverse_util.flatten_dict(params)
+        
+    #     flat_list = []
+    #     for _, v in flat_params.items():
+    #         flat_list.extend(jnp.ravel(v).tolist())  
+        
+    #     return jnp.array(flat_list)
+
+    def flatten_params(self,params):
+        leaves, _ = jax.tree_util.tree_flatten(params)
+        flat = jnp.concatenate([jnp.ravel(p) for p in leaves])
+        return flat
+
+    #@nnx.jit  # automatic state management for JAX transforms
+    def train_step(self,model_T, optimizer, W):
+        def loss_fn(model_T):
+            X = vmap(lambda w : model_T(w))(W)
             return self.KGD.evaluate(X)
 
-    def run(self,key,eta,T,n_sim=100):
-            Keys = random.split(key, T+1)
-            param0 = self.model_T.init(Keys[-1], jnp.zeros((self.d,)))
-            theta,_ = ravel_pytree(param0)
-            Theta = jnp.zeros((T, len(theta)))
-            Theta = Theta.at[0].set(theta)
-            for it in range(1,T):
-                key = Keys[0]#it]
-                DF = grad(lambda theta : self.F(theta, key, n_sim, self.d))
-                noise = 0 #1e-7 * random.normal(key+10000, shape=theta.shape)
-                theta = theta - eta * DF(theta + noise)
-                Theta = Theta.at[it].set(theta)
-            return Theta
+        loss, grads = nnx.value_and_grad(loss_fn)(model_T)
+        self.optimizer.update(grads)  # in-place updates
+        params = nnx.state(model_T, nnx.Param)
+        return self.flatten_params(params) #jnp.ones(self.d)#self.flatten_params_to_array(model_T)
+    
+    def run(self,T,key,n_sim = 100):
+        Keys = random.split(key, T)
+        params = nnx.state(self.model_T, nnx.Param)
+        flat_param0 = self.flatten_params(params)
+        dim_theta = flat_param0.shape[0]
+        Theta = jnp.zeros((T,dim_theta ))
+        for it in range(T):
+            key = Keys[it]
+            W = random.normal(key, shape=(n_sim, self.d))
+            theta = self.train_step(self.model_T, self.optimizer, W)
+            Theta = Theta.at[it].set(theta)
+        return Theta
+            
+
+
+    
+    # def push_T_theta(self,theta,W):
+    #      param = self.unravel_fn(theta)
+    #      return vmap(lambda w : self.model_T.apply(param, w))(W)
+        
+    # def F(self,theta,key,n_sim,d):
+    #         W = random.normal(key, shape=(n_sim, d))
+    #         X = self.push_T_theta(theta,W)
+    #         return self.KGD.evaluate(X)
+
+    # def run(self,key,eta,T,n_sim=100):
+    #         Keys = random.split(key, T+1)
+    #         param0 = self.model_T.init(Keys[-1], jnp.zeros((self.d,)))
+    #         theta,_ = ravel_pytree(param0)
+    #         Theta = jnp.zeros((T, len(theta)))
+    #         Theta = Theta.at[0].set(theta)
+    #         for it in range(1,T):
+    #             key = Keys[0]#it]
+    #             DF = grad(lambda theta : self.F(theta, key, n_sim, self.d))
+    #             noise = 0 #1e-7 * random.normal(key+10000, shape=theta.shape)
+    #             theta = theta - eta * DF(theta + noise)
+    #             Theta = Theta.at[it].set(theta)
+    #         return Theta
 
 
 
@@ -60,3 +134,28 @@ class VariationalInference:
 
     
 
+# model = Model(2, 64, 3, rngs=nnx.Rngs(0))  # eager initialization
+# optimizer = nnx.Optimizer(model, optax.adam(1e-3), wrt=nnx.Param)
+
+
+# @nnx.jit  # automatic state management for JAX transforms
+# def train_step(model, optimizer, x, y):
+#     def loss_fn(model):
+#         y_pred = model(x)  # call methods directly
+#         return ((y_pred - y) ** 2).mean()
+
+#     loss, grads = nnx.value_and_grad(loss_fn)(model)
+#     optimizer.update(model, grads)  # in-place updates
+
+#     return loss
+
+# @nnx.jit  # automatic state management for JAX transforms
+# def train_step(model_T, optimizer, W):
+#     def loss_fn(model_T):
+#         X = vmap(lambda w : model_T(w))(W)
+#         return self.KGD.evaluate(X)
+
+#     loss, grads = nnx.value_and_grad(loss_fn)(model_T)
+#     optimizer.update(model_T, grads)  # in-place updates
+
+#     return loss
